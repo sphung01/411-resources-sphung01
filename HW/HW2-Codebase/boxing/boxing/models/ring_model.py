21d8859b75691bf9dd9490347303f5677d35efd0
Steven Phung <sphung01@bu.edu>
2025-03-27 20:19:46 -0400

    Documented fight() in ring_model

--- HW/HW2-Codebase/boxing/boxing/models/ring_model.py
+++ HW/HW2-Codebase/boxing/boxing/models/ring_model.py
@@ -13,9 +13,23 @@ configure_logger(logger)
 
 class RingModel:
     def __init__(self):
+        """
+        Automatically initalizes the ring object
+        """
         self.ring: List[Boxer] = []
 
     def fight(self) -> str:
+        """
+        Path Parameter:
+            - self: An object that has its own ring data
+
+        Returns: 
+            - The winner in the fight 
+
+        Raises:
+            - If there are more than 2 boxers in the ring
+
+        """
         if len(self.ring) < 2:
             raise ValueError("There must be two boxers to start a fight.")
 
@@ -29,19 +43,26 @@ class RingModel:
         delta = abs(skill_1 - skill_2)
         normalized_delta = 1 / (1 + math.e ** (-delta))
 
+        logger.info(f"Generating random number")
         random_number = get_random()
 
         if random_number < normalized_delta:
             winner = boxer_1
             loser = boxer_2
+            logger.info(f"{boxer_2.name} lost! {boxer_1.name} is the winner!")
         else:
             winner = boxer_2
             loser = boxer_1
+            logger.info(f"{boxer_1.name} lost! {boxer_2.name} is the winner!")
 
+        logger.info(f"Updating both of the boxers' stats...")
         update_boxer_stats(winner.id, 'win')
         update_boxer_stats(loser.id, 'loss')
+        logger.info(f"Record updated!")
 
+        logger.info(f"Clearing the ring...")
         self.clear_ring()
+        logger.info(f"Ring is now cleared!")
 
         return winner.name
 
@@ -71,9 +92,11 @@ class RingModel:
             Error if the boxing ring is empty
         """
         if not self.ring:
-            raise ValueError("Ring is empty. Cannot return boxers")
+            pass
         else:
-            return self.ring
+            pass
+
+        return self.ring
 
     def get_fighting_skill(self, boxer: Boxer) -> float:
         # Arbitrary calculations
