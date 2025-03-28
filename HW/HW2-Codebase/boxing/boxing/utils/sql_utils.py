86ca639ce8865bf1c5870511bb2d4a8589bb4b51
Steven Phung <sphung01@bu.edu>
2025-03-28 17:09:13 -0400

    Just pushing to git

--- HW/HW2-Codebase/boxing/boxing/utils/sql_utils.py
+++ HW/HW2-Codebase/boxing/boxing/utils/sql_utils.py
@@ -11,7 +11,7 @@ configure_logger(logger)
 
 
 # load the db path from the environment with a default value
-DB_PATH = os.getenv("DB_PATH", "/app/sql/boxing.db")
+DB_PATH = os.path.join("DB_PATH", "app/sql/boxing.db")
 
 
 def check_database_connection():
